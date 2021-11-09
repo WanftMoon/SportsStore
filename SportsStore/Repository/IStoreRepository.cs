66492b823633047e30bf83866e7ea66d98f82581
Waner Tavares <wanft@moonlighters.com.br>
2021-11-09 20:24:55 -0300

    included crud operations for products.

--- SportsStore/Repository/IStoreRepository.cs
+++ SportsStore/Repository/IStoreRepository.cs
@@ -9,5 +9,9 @@ namespace SportsStore.Repository
 	public interface IStoreRepository
 	{
 		IQueryable<Product> Products { get; }
+
+		void SaveProduct(Product p);
+		void CreateProduct(Product p);
+		void DeleteProduct(Product p);
 	}
 }
--- SportsStore/Repository/StoreRepository.cs
+++ SportsStore/Repository/StoreRepository.cs
@@ -16,5 +16,22 @@ namespace SportsStore.Repository
 		}
 
 		public IQueryable<Product> Products => _context.Products;
+
+		public void CreateProduct(Product p)
+		{
+			_context.Add(p);
+			_context.SaveChanges();
+		}
+
+		public void DeleteProduct(Product p)
+		{
+			_context.Remove(p);
+			_context.SaveChanges();
+		}
+
+		public void SaveProduct(Product p)
+		{
+			_context.SaveChanges();
+		}
 	}
 }
