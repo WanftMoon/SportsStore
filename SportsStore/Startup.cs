a3147596c3716d0f4e4c332d0132d18fb589f44b
Waner Tavares <wanft@moonlighters.com.br>
2021-11-11 20:28:25 -0300

    added the authorize att added the login page added the error page updated the startup to display simplified error page in production env

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -63,9 +63,14 @@ namespace SportsStore
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
+			if (env.IsProduction())
+			{
+				app.UseExceptionHandler("/error");
+			}
+			else
 			{
 				app.UseDeveloperExceptionPage();
+				app.UseStatusCodePages();
 			}
 
 			app.UseStatusCodePages();
